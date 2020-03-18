585a2fdcb58f240bfd580a0cec1dd560f42be446
Zoe Ludwig <zoe.ludwig@lambdaschool.com>
2020-03-18 11:33:09 -0700

    deleted SARS

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
-// Task 1: Map the data to a new array with just country and number of deaths 
+// ⭐️ Example Challenge START ⭐️
 
-// Task 2: Filter out any objects where the number of deaths is 0 
 
-// Task 3: Create new arrays for each Country containing just the number of deaths
-// Hint: Use control flow along with a .forEach loop
-// Hint: This may take more than one step! 
 
-// Task 4: use a .reduce method to determine how many people died from SARS in each country
+// Task 1: examine data
+
+/* Task 2:  getFinals takes one argument:
+ *     (1) an array which is contains all world cup game data
+ * 
+ * getFinals returns an array of objects with only finals data */
+
+function getFinals(/* code here */) {
+
+};
+
+/* Task 3: Using the return from `getFinals`, create an array called `years` and fill it with all of the years in the dataset */
+
+let finals = getFinals();
+
+function getYears(/* code here */) {
+
+};
+
+/* Task 4: Using the return from `getFinals`,determine the winner (home or away) of each `finals` game and add the country name to a new `winners` array */
+
+function getWinners(/* code here */) {
+
+};
+
+/* Task 5: Implement a higher-order function called `getAllWinners`.
+ * It takes three arguments:
+ * @param an array of years.
+ * @param an array of winners.
+ * @param callback function that takes years and winners as its arguments
+ * @returns a set of strings "In {year}, {country} won the world cup!" */
+
+function getAllWinners(/* code here */) {
+
+};
+
+// run your higher order function
+getAllWinners();
