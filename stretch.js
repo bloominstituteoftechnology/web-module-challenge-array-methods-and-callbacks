3203df502e46fbea636d1b2f13709da1a34178e2
Joey <joey@xm8.ninja>
2020-04-20 22:18:41 -0400

    Added stretch goal file

---
+++ stretch.js
@@ -0,0 +1,16 @@
+import { fifaData } from "./fifa.js";
+console.log(fifaData);
+
+// Create a function that takes country initials as a parameter and returns their total number of World Cup appearances.
+
+var fifaHomeInitials = fifaData.map(function (item) {
+  return item["Home Team Initials"];
+});
+
+var fifaAwayInitials = fifaData.map(function (item) {
+  return item["Away Team Initials"];
+});
+
+var fifaInitials = fifaHomeInitials.concat(fifaAwayInitials);
+
+console.log(fifaInitials);
