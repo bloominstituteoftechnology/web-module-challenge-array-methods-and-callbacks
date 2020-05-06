dfdbcf933644405c1ed20eace1f3cae39ff2a838
unknown <clay2000lbs@gmail.com>
2020-05-05 20:46:23 -0500

    Start of assignment

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ console.log(fifaData);
 (c) Home Team goals for 2014 world cup final
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
-
+console.log(fifaData[year:2014][name])
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
 function getFinals(/* code here */) {
 
-    /* code here */
+    
 
 };
 
