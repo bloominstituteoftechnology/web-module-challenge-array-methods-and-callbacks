918888cb205578deb7681afc1e2f588fb951d262
unknown <admin@social-collab.org>
2020-05-19 22:12:05 -0400

    Completed Task 3

--- index.js
+++ index.js
@@ -2,36 +2,86 @@ import { fifaData } from './fifa.js';
 console.log(fifaData);
 
 console.log('its working');
+
+// DID NOT COMPLETE. WILL NEED ADDITIONAL TIME.
 // ⚽️ M  V P ⚽️ //
 
 /* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
 
 (a) Home Team name for 2014 world cup final
+
 (b) Away Team name for 2014 world cup final
 (c) Home Team goals for 2014 world cup final
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
+let i;
+function homeTeamA(fifaData){
+
+    for(i = 0; i <= fifaData.length - 1; i++) {
+        if((fifaData[i].Year === 2014) && (fifaData[i].Stage === 'Final')) {
+            let fifaDataA = fifaData[i];
+            if(fifaDataA['Home Team Goals'] > fifaDataA['Away Team Goals']) {
+                var winnerA = fifaDataA['Home Team Name'];
+            } else {
+                var winnerA = fifaDataA['Away Team Name'];
+            }
+            if(fifaDataA['Home Team Goals'] === 1) {
+            var pluralOrSingularA = 'goal'; // uses proper speech for 1 goal (singular)
+            } else {
+                var pluralOrSingularA = 'goals'; // uses proper speech for 0, or 2 and up goals (plural)
+            }
+            if(fifaDataA['Away Team Goals'] === 1) {
+                var pluralOrSingularB = 'goal';
+            } else {
+                var pluralOrSingularB = 'goals';
+            }
+            return `Home Team: ${fifaDataA['Home Team Name']} scored ${fifaDataA['Home Team Goals']} ${pluralOrSingularA} and Away Team: ${fifaDataA['Away Team Name']} scored ${fifaDataA['Away Team Goals']} ${pluralOrSingularB}. ${winnerA} won the game`;
+        }
+    }
+};
+console.log(homeTeamA(fifaData));
 
 
-/* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
+/* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only
+finals data */
 
-function getFinals(/* code here */) {
+const finalsData = [];
 
-    /* code here */
+function getFinals(fifaData) {
 
+    for(i = 0; i <= fifaData.length - 1; i++) {
+        if(fifaData[i].Stage === 'Final') {
+            finalsData.push(fifaData[i]);
+        }
+    }
+    return finalsData;
 };
 
-/* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
+console.log(getFinals(fifaData));
 
-function getYears(/* code here */) {
+/* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns
+an array called `years` containing all of the years in the dataset */
+// http://127.0.0.1:5500/
 
-    /* code here */
+const theYears = [];
 
+function getYears(fifaData) {
+
+    for(i = 0; i <= fifaData.length - 1; i++) {
+        if(fifaData[i].Stage === 'Final') {
+            theYears.push(fifaData[i].Year);
+        }
+    }
+    return theYears;
 };
 
-getYears();
+console.log(getYears(fifaData));
 
-/* Task 5: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
+// Could not complete Task 3 using a callback function. Completed it this way in order to move on.
+
+/* Task 5: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and
+determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called
+`winners` */ 
 
 function getWinners(/* code here */) {
 
