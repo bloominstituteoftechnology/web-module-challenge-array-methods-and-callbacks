bc1cb687ab504c37a5c7bcc80cf9d658ec53686f
eric capiz <ericcapiz@gmail.com>
2020-06-03 22:59:45 -0500

    task 1 complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { fifaData } from './fifa.js';
-console.log(fifaData);
+// console.log(fifaData);
 
-console.log('its working');
+// console.log('its working');
 // ⚽️ M  V P ⚽️ //
 
 /* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
@@ -11,6 +11,14 @@ console.log('its working');
 (c) Home Team goals for 2014 world cup final
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
+
+const final2014 = fifaData.filter((game)=>{
+    return game.Year == 2014 && game.Stage == "Final";
+});
+console.log(final2014[0]["Home Team Name"]);
+console.log(final2014[0]['Away Team Name']);
+console.log(final2014[0]['Home Team Goals']);
+console.log(final2014[0]['Away Team Goals']);
 
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
