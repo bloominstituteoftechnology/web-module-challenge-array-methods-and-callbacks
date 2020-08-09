eeb965d53b7962e57370b1f219c6d62ef94e96bc
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-08-09 03:29:47 -0700

    Finals with includes and filter a substr

--- index.js
+++ index.js
@@ -44,12 +44,16 @@ task1();
 /* Task 2: Create a function called  getFinals that takes 
 `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals() {
-
+function getFinals(data) {
+    let a=[{}];
+    return a = data.filter(e => 
+       e.Stage.includes('finals',0)
+        );
   
 
 };
-
+let aob = getFinals(fifaData);
+console.log(aob);
 /* Task 3: Implement a higher-order function called `getYears` 
 that accepts the callback function `getFinals`, and returns an array called
  `years` containing all of the years in the dataset */
