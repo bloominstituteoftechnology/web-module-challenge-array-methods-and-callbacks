66f355b9fea48fb13a287a9e589709f1e019d2d7
Andy Sakai <andy.sakai@outlook.com>
2020-08-12 10:40:24 -0700

    Need to finish

--- index.js
+++ index.js
@@ -4,38 +4,61 @@ console.log(fifaData);
 console.log('its working');
 // ⚽️ M  V P ⚽️ //
 
-/* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
+//Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
 
-(a) Home Team name for 2014 world cup final
-(b) Away Team name for 2014 world cup final
-(c) Home Team goals for 2014 world cup final
-(d) Away Team goals for 2014 world cup final
-(e) Winner of 2014 world cup final */
+//(a) Home Team name for 2014 world cup final
+
+//console.log("Home team: " + fifaData)
+
+//(b) Away Team name for 2014 world cup final
+//(c) Home Team goals for 2014 world cup final
+//(d) Away Team goals for 2014 world cup final
+//(e) Winner of 2014 world cup final */
 
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
+/*
+function getFinals(arr) {
 
-function getFinals(/* code here */) {
+   var finals = [];
 
-    /* code here */
+   for(i =0; i<arr.length; i++)
+   {
+        if(arr.stage = "Final")
+        {
+            finals.push(arr[i]);
+        }
+        
+        return finals;
 
+   }
 };
 
+  console.log(getFinals(fifaData))
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
+/*
+function getYears(arr, callback) {
 
-function getYears(/* code here */) {
 
-    /* code here */
+    funct(arr.years)
 
 };
 
-getYears();
+getYears(fifaData, (getFinals) => {
+
+   
+
+});
+
+
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
+/*
+function getWinners(funct) {
 
-function getWinners(/* code here */) {
+    var winners = []
 
-    /* code here */
+    for(i = 0; i <)
 
 };
 
