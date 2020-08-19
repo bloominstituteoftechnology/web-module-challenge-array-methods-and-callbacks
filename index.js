9b889b88a2446d9b1a95dca7242bfc33a064048f
Simone Ballard <simone.a.ballard@gmail.com>
2020-08-19 13:43:29 -0600

    Task 2 completed not tested yet due to random error

--- index.js
+++ index.js
@@ -58,11 +58,13 @@ console.log(getWinnerTeam(fifaData, 2014));
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
+function getFinals(array) {
+    const finalsdata = array.filter(function(item){
+        return item.stage = "Group 4";
+}
+}
 
-    /* code here */
-
-};
+console.log(getFinals(fifaData));
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
