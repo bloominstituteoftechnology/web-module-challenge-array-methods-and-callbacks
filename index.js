06e3a1cb786b716d24fcf7dd0117f462c5502b49
Samantha <barney-samantha@lambdastudents.com>
2020-10-02 23:26:08 -0400

    task 1-2 done

--- index.js
+++ index.js
@@ -2,24 +2,58 @@ import { fifaData } from './fifa.js';
 console.log(fifaData);
 
 console.log('its working');
-// ⚽️ M  V P ⚽️ //
 
-/* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
 
-(a) Home Team name for 2014 world cup final
-(b) Away Team name for 2014 world cup final
-(c) Home Team goals for 2014 world cup final
-(d) Away Team goals for 2014 world cup final
-(e) Winner of 2014 world cup final */
+
+// Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
+
+// (a) Home Team name for 2014 world cup final
+
+console.log(fifaData.length - 1 ['2014 Home Team']);
+
+// (b) Away Team name for 2014 world cup final
+
+console.log(fifaData.length - 1 ['2014 Away Team']);
+
+// (c) Home Team goals for 2014 world cup final
+
+console.log(fifaData.length - 1 ['2014 Home Goals']);
+
+//(d) Away Team goals for 2014 world cup final
+
+console.log(fifaData.length - 1 ['2014 Away Goals']);
+
+// (e) Winner of 2014 world cup final 
+
+function CupWinner2014(home, away) {
+
+    if (home > away) {
+        console.log(fifaData.length - 1 ['2014 Home Team'] + 'is the winner!');
+    }else{
+        console.log(fifaData.length - 1 ['2014 Away Team'] + 'is the winner!');
+        }
+    }
+
+2014CupWinner(fifaData.length - 1 ['2014 Home Goals'], fifaData.length - 1 ['2014 Away Goals']);
+    
+console.log(CupWinner2014);
 
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
 
-    /* code here */
 
-};
+const getFinals = fifaData.filter((item) => {
+    return(item['Stage'] === 'Final');
+});
+
+console.log(getYears);
+
+
+
+
+
+
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
@@ -63,6 +97,33 @@ function getAverageGoals(/* code here */) {
 };
 
 getAverageGoals();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /// STRETCH 🥅 //
 
