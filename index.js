672f228905638f8fea01847c796f54134200e2c5
Matthew Larson <maflarson@gmail.com>
2020-10-13 11:53:50 -0600

    a;lsdkfj

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ console.log('its working');
 /* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
 
 (a) Home Team name for 2014 world cup final
+
 (b) Away Team name for 2014 world cup final
+
 (c) Home Team goals for 2014 world cup final
+
 (d) Away Team goals for 2014 world cup final
+
 (e) Winner of 2014 world cup final */
+
 
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
