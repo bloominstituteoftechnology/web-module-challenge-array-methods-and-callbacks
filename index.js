b1da2527518fa9d91a1b9cf7b4cfc6f95374ca6d
Ethan Karp <ethansmainemail@gmail.com>
2020-11-01 14:56:35 -0700

    finished task 2

--- index.js
+++ index.js
@@ -67,18 +67,16 @@ console.log(winner);
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-// function getFinals(array) {
-//     let narr = [];
-//     for (let i = 0; i < array.length; i++) {
-//         if (array[i]["Stage"] === "Final") {
-//             narr.push(array[i]);
-//         }
-//     }
+function getFinals(array) {
 
-//     return narr;
-// };
+    let narr = array.filter((item) => {
+        return item["Stage"] === "Final";
+    })
 
-// console.log(getFinals(fifaData));
+    return narr;
+};
+
+console.log(getFinals(fifaData));
 
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
