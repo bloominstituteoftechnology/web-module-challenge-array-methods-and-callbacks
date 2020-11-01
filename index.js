8f6642f7bd9146d0d600d76224cf6e35dcd7704f
daniel-vazquez <da.vazquez728@gmail.com>
2020-11-01 15:03:26 -0700

    completed task 3

--- index.js
+++ index.js
@@ -28,13 +28,17 @@ console.log(getFinals(fifaData));
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(cb) {
-
+function getYears( cb ) {
+    const finalsCb = cb;
+    const finalsYear = finalsCb.map(function (item) {
+        return item.Year;
+    });
+        return finalsYear;
     
-
 };
 
-getYears();
+console.log(getYears(getFinals(fifaData)));
+
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
