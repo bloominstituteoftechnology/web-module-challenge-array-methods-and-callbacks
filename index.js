1d27b942e541283b4b60b90ed4d176593f1c3998
William Dye <williamdye@Williams-MacBook-Pro.local>
2020-11-10 22:49:02 -0500

    got some help but mostly lost

--- index.js
+++ index.js
@@ -7,23 +7,58 @@ import { fifaData } from './fifa.js';
 Investigate the data above. Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
 
 //(a) Home Team name for 2014 world cup final
+const final2014 = fifaData.filter((match) => {
+   return match.year === 2014 && match.stage === "Final" // conditional
+})
+/*homeTeam2014 = [
+    {
+    "Year": 2014, //randomword
+    "Datetime": "13 Jul 2014 - 16:00",
+    "Stage": "Final",
+    "Stadium": "Estadio do Maracana",
+    "City": "Rio De Janeiro",
+    "Home Team Name": "Germany",
+    "Home Team Goals": 1,
+    "Away Team Goals": 0,
+    "Away Team Name": "Argentina",
+    "Win conditions": "Germany win after extra time",
+    "Attendance": 74738,
+    "Half-time Home Goals": 0,
+    "Half-time Away Goals": 0,
+    "Referee": "Nicola RIZZOLI (ITA)",
+    "Assistant 1": "Renato FAVERANI (ITA)",
+    "Assistant 2": "Andrea STEFANI (ITA)",
+    "RoundID": 255959,
+    "MatchID": 300186501,
+    "Home Team Initials": "GER",
+    "Away Team Initials": "ARG"
+    }
+] */
+console.log(final2014[0]['Home Team Data']);
+
+
 
 //(b) Away Team name for 2014 world cup final
-
+// const awayTeam2015 = fifaData.filter(match2 => {
+//     return //to locate data in object = conditional
+//     [awayTeam2015[0]["Away Team Name"] [//access the object and pull out value associated with the away team key
+// })
+console.log(final2014[0]["Away Team Data"]);
 //(c) Home Team goals for 2014 world cup final
-
+console.log(final2014[0]["Home Team Goals"]);
 //(d) Away Team goals for 2014 world cup final
-
+console.log(final2014[0]["Away Team Goals"]);
 //(e) Winner of 2014 world cup final */
+console.log(final2014[0]["Home Team Name"]);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-   /* code here */
+const func = getFinals(data) = {
+    fifaData.filter((match) => {
+        return match.year === 2014 && match.stage === "Final"
 }
-
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
