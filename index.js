38ce0ece17bd7031803097a2b05ae60d6bcdda08
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2020-12-03 19:39:30 -0500

    first part

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ import { fifaData } from './fifa.js';
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
 
+const finals2014 = fifaData.filter(function(item){
+    return item.Year === 2014 && item.Stage === "Final"
+})
+
 //(a) Home Team name for 2014 world cup final
-
+console.log(finals2014[0]["Home Team Name"])
 //(b) Away Team name for 2014 world cup final
-
+console.log(finals2014[0]["Away Team Name"])
 //(c) Home Team goals for 2014 world cup final
-
+console.log(finals2014[0]["Home Team Goals"])
 //(d) Away Team goals for 2014 world cup final
-
+console.log(finals2014[0]["Away Team Goals"])
 //(e) Winner of 2014 world cup final */
-
+console.log(finals2014[0]["Win Conditions"])
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use getFinals to do the following:
@@ -24,11 +28,17 @@ Use getFinals to do the following:
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
-   /* code here */
-}
-
-
+function getFinals(fifadata) {
+   
+    const getFinals = fifadata.filter(function(item){
+       
+       
+        return item.Stage === "Final";  
+    
+    }); 
+    
+    console.log(getFinals);
+} 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function called getYears to do the following: 
@@ -36,10 +46,15 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
-}
+function getYears(fifadata) {
+    const getYears = fifadata.map(function(item){
 
+    return item.Year;
+
+    });
+    return getYears
+}
+getYears(getFinals)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
