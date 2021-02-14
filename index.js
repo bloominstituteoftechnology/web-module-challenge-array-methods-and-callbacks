4778ed4c05c9cfcc923cc8d73738960028624d52
Hansel Valentine <hanselviva@gmail.com>
2021-02-14 14:27:47 -0800

    after 3.5 hours, I finally did Task 6 to work. Done with MVP.

--- index.js
+++ index.js
@@ -135,8 +135,12 @@ Use the higher order function getAverageGoals to do the following:
  Example of invocation: getAverageGoals(getFinals(fifaData));
 */
 
-function getAverageGoals(/* code here */) {
-	/* code here */
+function getAverageGoals(getFinals) {
+	const grandTotal = getFinals.reduce(
+		(total, item) => total + item["Home Team Goals"] + item["Away Team Goals"],
+		0,
+	);
+	return (grandTotal / getFinals.length).toFixed(2);
 }
 
 /// 🥅 STRETCH 🥅 ///
