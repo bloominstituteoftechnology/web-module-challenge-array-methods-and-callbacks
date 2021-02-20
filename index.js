29d24b29cea5aa785d666f633740f0b4973b796a
Wahajuddin Nayyar <Wahajuddin_Nayyar@outlook.com>
2021-02-20 04:00:32 -0500

    currently working on task 5

--- index.js
+++ index.js
@@ -80,8 +80,8 @@ Use the higher-order function getWinnersByYear to do the following:
 hint: the strings returned need to exactly match the string in step 4.
  */
 
-function getWinnersByYear(/* code here */) {
-    /* code here */
+function getWinnersByYear(anArr, cbOne, cbTwo) {
+    
 }
 
 
