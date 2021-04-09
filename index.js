ea256ae7f3baa395fcd12d8f99f64701981dbe0a
Amlak befekadu <amlakbarkon@gmail.com>
2021-04-08 23:54:30 -0400

    q1 is done

--- index.js
+++ index.js
@@ -6,28 +6,41 @@ import { fifaData } from './fifa.js';
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
 
 //(a) Home Team name for 2014 world cup final
-
+   let result = fifaData.filter((x)=> x.Year == 2014);
+    // console.log("Result 2014 "+ result.length, '\n', result)
+   let homeTeam = result.map((x,i)=> {return(x.Year + " Home Team " + (i+1) + " is " + x['Home Team Name'] +'\n')});
+    //   console.log("List of Home Team" +'\n'+ homeTeam)
 //(b) Away Team name for 2014 world cup final
-
+    let awayTeam = result.map((x,i)=> {return(x.Year," Away Team " + i + " is " + x['Away Team Name']+ '.\n')});
+    //    console.log("List of Away Team" + "\n" + awayTeam)
 //(c) Home Team goals for 2014 world cup final
-
+    let homeTeamGoals = result.map(x=> {return x['Home Team Goals']})
+    //    console.log(`Home Team Goals ${homeTeamGoals}`)
 //(d) Away Team goals for 2014 world cup final
-
+let awayTeamGoals = result.map(x=> {return x['Away Team Goals']})
+    //    console.log(`Away Team Goals ${awayTeamGoals}`)
 //(e) Winner of 2014 world cup final */
-
-
+let winner = result.filter(x=> {return x['Stage'] =='Final'})
+    //    let winner2014=winner.filter(x=>x["Stage"]= "Final)
+       if(winner[0]["Home Team Goals"]> winner[0]["Away Team Goals"]){
+        //    console.log(winner[0]["Home Team Initials"])
+       }else {
+            //  console.log(winner[0]["Away Team Initials"])
+       }
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use getFinals to do the following:
 1. Receive data as a parameter
 2. Return an array of objects with the data of the teams that made it to the final stage
-
+  
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
-   /* code here */
+function getFinals(array) {
+  let result = array.filter(x=> x.Stage == "Final")
+  
+  return result;
 }
-
+console.log(getFinals(fifaData));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -36,10 +49,12 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(array, callback) {
+    callback((x)=>{
+     
+    })
 }
-
+getYears()
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
