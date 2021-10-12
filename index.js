cd9ec586a595f85e13980bbb88eb10b96ac39306
Britt Dalby <91482954+brittney-dalby@users.noreply.github.com>
2021-10-12 14:17:20 -0600

    working through task 3

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const { fifaData } = require('./fifa.js')
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
 const finals2014 = fifaData.filter(function(item){
-    return item['Year'] === 2014 && item['Stage'] === 'Final';
+    return item.Year === 2014 && item.Stage === 'Final';
 });
 console.log(finals2014);
 //(a) Home Team name for 2014 world cup final
-console.log('task 1a', finals2014[0]['Home Team Name']);
-
+console.log(finals2014[0]['Home Team Name']);
 //(b) Away Team name for 2014 world cup final
 console.log(finals2014[0]['Away Team Name']);
 //(c) Home Team goals for 2014 world cup final
@@ -18,7 +17,7 @@ console.log(finals2014[0]['Home Team Goals']);
 //(d) Away Team goals for 2014 world cup final
 console.log(finals2014[0]['Away Team Goals']);
 //(e) Winner of 2014 world cup final */
-console.log(finals2014[0]['Win Conditions']);
+console.log(finals2014[0]['Win conditions']);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use getFinals to do the following:
@@ -28,10 +27,13 @@ Use getFinals to do the following:
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
-   /* code here */
+function getFinals(data) {
+   const finalTeams = data.filter(function(item){
+       return item.Stage === 'Final';
+   })
+   return finalTeams;
 }
-
+console.log(getFinals(fifaData));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -40,10 +42,12 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(data, getFinalsCB) {
+    return getFinalsCB(data).map(function(item){
+        return item.Year;
+    });
 }
-
+console.log(getYears(fifaData, getFinals));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
